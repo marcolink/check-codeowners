5709d769c3d1062f7eb254ad39d6fbf51fc1cf6c
Marco Link <marco.link@contentful.com>
2020-05-04 09:37:58 +0200

    feat(chore): improve output

--- src/check-coverage.ts
+++ src/check-coverage.ts
@@ -1,5 +1,5 @@
-import {CodeOwners} from "./parse-code-owners";
 import micromatch from "micromatch";
+import {CodeOwners} from "./parse-code-owners";
 
 const checkCoverage = (files: string[], codeowners: CodeOwners): string[] => {
   return codeowners.reduce((matched: string[], codeOwnersEntry) =>
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import {Command, flags} from '@oclif/command'
+import chalk from "chalk";
 import * as fs from "fs-extra";
 import * as path from "path";
 import recursive from "recursive-readdir";
-import {parseCodeOwners} from "./parse-code-owners";
+import {table} from "table";
 import {checkCoverage} from "./check-coverage";
+import {parseCodeOwners} from "./parse-code-owners";
 
 class CheckCodeOwners extends Command {
   static description = 'check codeowners project coverage';
@@ -26,16 +28,17 @@ class CheckCodeOwners extends Command {
       return;
     }
 
+    const valueFormat = chalk.yellow;
     const files = await recursive(project);
-    console.log(`${files.length} files found`);
-
     const parsedCodeOwners = await parseCodeOwners((await fs.readFile(codeOwnersFilePath)).toString());
-    console.log(`${parsedCodeOwners.length} codeowners pattern found`);
+    const content = parsedCodeOwners.map(entry => [...Object.values(entry)
+      .map((value) => valueFormat(value))]);
 
     const coveredFiles = checkCoverage(files, parsedCodeOwners);
     const percentage = ((coveredFiles.length / files.length) * 100).toFixed(2);
-    console.log(`${coveredFiles.length} (${percentage}%) files covered by codeowners`);
 
+    console.log(table([[chalk.bold('pattern'), chalk.bold('codeowners')], ...content], {}));
+    console.log(`${valueFormat(coveredFiles.length)}/${valueFormat(files.length)} ${valueFormat(percentage + '%')} files covered by codeowners`);
   }
 }
 
